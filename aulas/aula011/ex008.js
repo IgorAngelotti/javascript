a47b3e74871ffc8be2261051f218b8628c0cb42a
Igor Angelotti <105242564+IgorAngelotti@users.noreply.github.com>
2022-07-04 16:17:55 -0300

    condicionais

---
+++ aulas/aula011/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}`)
+if (vel > 60 ) { //CONDIÇÃO SIMPLES
+    console.log('Você ultrapassou a velocidade permitida! MULTADO.')
+}
---
+++ aulas/aula011/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log (`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log ('Eu sou Brasileiro')
+} else{
+    console.log ('Você é estrangeiro')
+}
