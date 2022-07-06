06a0e1f725086e6c00283b38b63e1f51972d9eb5
Igor Angelotti <105242564+IgorAngelotti@users.noreply.github.com>
2022-07-06 12:00:27 -0300

    aula013

---
+++ aulas/aula013/teste.js
@@ -0,0 +1,17 @@
+
+//TESTES COM A ESTRUTURA DE REPETIÇÃO
+
+var num = 1
+
+/* WHILE
+while (num <= 10){
+    console.log(`oi ${num}`)
+    num ++
+}
+*/
+
+// DO...WHILE
+do{
+    console.log(`oi ${num}`)
+    num ++ 
+} while (num <= 10)
