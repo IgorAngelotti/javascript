a4cf84bb825e37285aa1ad2bdca0dfbd61e1651e
Igor Angelotti <105242564+IgorAngelotti@users.noreply.github.com>
2022-07-11 20:36:16 -0300

    exercicio

--- aulas/aula016ex/modelo/script.js
+++ aulas/aula016ex/modelo/script.js
@@ -22,17 +22,50 @@ function inLista(n, l){
 
 function adicionar(){
    if (isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert('tudo ok')
+        
+    //USAR O DO... WHILE
+        
+        var numero = Number(num.value)
+        valores.push(numero)
+        let item = document.createElement('option')
+        item.text = `Valor ${numero} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
    } else {
         window.alert('Valor inválido ou já encontrado na lista')
    }
-   
-   
-   
+   num.value = ''
+   num.focus()
+}
+
+function finalizar (){
+    var soma = 0
+    var ultimo = valores.length
+    valores.sort()
+    var mnum = valores[0]
+    var Mnum = valores[ultimo-1]
+    for (cont = 0; cont < valores.length; cont++){
+        soma += valores[cont]
+    }
+    var media = soma/valores.length
+    res.innerHTML = ''
+    if (valores.length == 0){
+        window.alert('Adicione algum número antes de finalizar')
+    } else if (valores.length == 1){
+        res.innerHTML += `<p> Ao todo, temos 1 único número cadastrado. Para mais dados, informe, no mínimo, dois números.</p>`
+    } else{
+    res.innerHTML += `<p> Ao todo, temos ${valores.length} números cadastrados</p>`
+    res.innerHTML += `<p> O menor valor foi ${mnum}</p>`
+    res.innerHTML += `<p> O maior valor foi ${Mnum} </p>`
+    res.innerHTML += `<p> A soma entre todos os valores digitados foi ${soma}`
+    res.innerHTML += `<p> A media dos valores digitados é ${media.toFixed(2)}`
+    }
+    
+
+}
     // valor = Number(num.value)
     // item.text = `Valor ${valor} adicionado`
     // cont.push(valor)
     // lista.appendChild(item)
     // var p = document.getElementById('p')
     
-}
