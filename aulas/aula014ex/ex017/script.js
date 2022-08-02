77552dc2c392b4e8480e1415723a4ad3bbe7dfa9
Igor Angelotti <105242564+IgorAngelotti@users.noreply.github.com>
2022-08-02 14:46:43 -0300

    atualizações

--- aulas/aula014ex/ex017/script.js
+++ aulas/aula014ex/ex017/script.js
@@ -1,16 +1,19 @@
+var botao = document.getElementById('botao')
+botao.addEventListener('click', calcular)
 function calcular(){
     var num = document.getElementById('numero')
     var cont = 0
-    var res = document.getElementById('res')
-    res.innerHTML = '' 
+    var flista = document.getElementById('lista')
+    flista.innerHTML = ''
     if (num.value.length == 0){
         res.innerHTML = 'É preciso informar um número!!'
     } else {
     var tabuada = Number(num.value)
-    res.innerHTML = `Tabuada do ${tabuada}:`
     while (cont <=10){
+        var item = document.createElement('option')
         var mult = tabuada * cont 
-        res.innerHTML += `<br>${tabuada} X ${cont} = ${mult} `
+        item.text = `${tabuada} X ${cont} = ${mult} `
+        flista.appendChild(item)
         cont ++
     }
     }
--- aulas/aula015/ambiente.js
+++ aulas/aula015/ambiente.js
@@ -1,10 +1,11 @@
 let num = [7, 5, 9, 3, 4, 0]
-num.push(6) //CRIA MAIS UMA CHAVE (A ULTIMA, QUE NESSE EXEMPLO SERIA A CHAVE 6) COM O VALOR = "7"
+num.push(6) //CRIA MAIS UMA CHAVE (A ULTIMA, QUE NESSE EXEMPLO SERIA A CHAVE 6) COM O VALOR = "6"
 num[7] = 8 //CRIA A CHAVE 7, COLOCANDO NELA O VALOR = "8"
 // num.length -- >QUANTOS ELEMENTOS O VETOR POSSUI 
 // num.sort() --> VAI ORDENAR EM ORDEM CRESCENTE OS ELEMENTOS
+var tamanho = num.length
 console.log(num)
-console.log(`O vetor tem ${num.length} elementos, sendo o primeiro valor ${num[0]} e o ultimo ${num[7]}.`)
+console.log(`O vetor tem ${num.length} elementos, sendo o primeiro valor ${num[0]} e o ultimo ${num[num.length - 1]}.`)
 console.log(`O vetor em ordem crescente fica: ${num.sort()} `)
 console.log()
 
